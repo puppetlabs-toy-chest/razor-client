df7b7dcdf1b72e9cc1a01cf88cc7c8e8ccd69ca0
Scott McClellan <scott.mcclellan@puppetlabs.com>
2015-04-27 14:00:57 -0500

    Fit collection views to terminal width
    If the user views a collection that has long widths for any of the columns, the
    results would wrap in a manner that was very hard to read. This change takes
    into account the size of the user's terminal window when rendering the table.
    
    Fixes https://tickets.puppetlabs.com/browse/RAZOR-571

--- lib/razor/cli/table_format.rb
+++ lib/razor/cli/table_format.rb
@@ -8,7 +8,7 @@ class Razor::CLI::TableFormat
       headings = (column_overrides or get_headers(doc))
       row do
         headings.each do |header|
-          column(header, :width => get_width(header, doc))
+          column(header, :width => column_width!(headings, header, doc))
         end
       end
       doc.each do |page|
@@ -23,12 +23,48 @@ class Razor::CLI::TableFormat
     capture_output.strip
   end
 
-  def get_width(header, doc)
+  # This method has the side effect of modifying the remaining extra_width.
+  # It pulls everything together to come up with a single value for the column.
+  def column_width!(headings, header, doc)
+    content_width = content_width(header, doc)
+    average_width = average_width(headings)
+    # Is the column too wide and can we do anything about it?
+    if content_width > average_width && extra_width(headings, doc) > 0
+      # Determine how much room we'd need to make to accommodate the content.
+      remaining = content_width - average_width
+      # Add back in what we can.
+      width = average_width + [@extra_width, remaining].min
+      # The new width can't be negative.
+      @extra_width = [@extra_width - remaining, 0].max
+      width
+    else
+      [content_width, average_width].min
+    end
+  end
+
+  # This calculates how much leeway would exist in all columns if we were to
+  # use an auto-sized fixed width for the whole table.
+  def extra_width(headings, doc)
+    @extra_width ||= headings.map do |header|
+                       [average_width(headings) - content_width(header, doc), 0].max
+                     end.inject(:+)
+  end
+
+  # This calculates what an auto-sized fixed-width table's column width would be.
+  def average_width(headings)
+    # The 3 here = 2 for width gap + 1 for the column separator.
+    # The 1 is for the last separator.
+    @average_width ||= (((`tput cols` || 80).to_i - (headings.count * 3) - 1) / headings.count)
+  end
+
+  def content_width(header, doc)
+    # Find longest item, including the header
     (doc.map do |page|
       (page[header] or '').to_s.length
     end << header.to_s.length).max
   end
 
+  # Traverse all headers to compile a unique list.
   def get_headers(doc)
     [].tap do |headers|
       doc.map do |page|
